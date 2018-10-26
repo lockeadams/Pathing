54454c5e42fe09e50666a66a0fbb7ceef06b6739
unknown <lockeadams@gmail.com>
2018-10-25 21:53:18 -0400

    Added smoothing method that is partially working. *Smooths a path that has been injected by averaging slopes *Current issues: doesn't relocate cusp and overshoots target Y (see output of current main method)
    Will try to address issues soon.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,15 @@
 public class Main {
-
-	static int test;
 	
 	public static void main(String[] args) {
 		
 		//new GUI();
-		
 		Path path = new Path();
 		path.addWaypoint(0, 0);
-		path.addWaypoint(10, 0);
-		path.addWaypoint(20, 10);
-		System.out.println(path.injectPoints(0.5));
-		
+		path.addWaypoint(0, 10);
+		path.addWaypoint(10, 10);
+		//System.out.println(path.injectPoints(0.5));
+		System.out.println(path.smooth(3)); 
+
 		
 	}
 }
--- src/main/java/Path.java
+++ src/main/java/Path.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Path extends ArrayList<Point2D> {
 	
 	private static final long serialVersionUID = 1L; //not sure why i had to add this
-	ArrayList<Point2D> waypoints, genPoints;
+	ArrayList<Point2D> waypoints, injectedPoints, smoothPoints;
 	Point2D normalizedP1, normalizedP2;
 	Vector normalized, spaced;
 	int numOfPoints;
@@ -22,8 +22,15 @@ public class Path extends ArrayList<Point2D> {
 		waypoints.add(newWaypoint);		
 	}
 	
+	/**
+	 * Takes Path and injects a new Path with a given amount of points 
+	 * based on spacing and distance between points in original path.
+	 * 
+	 * @author Locke Adams
+	 * @param spacing
+	 */
 	public ArrayList<Point2D> injectPoints(double spacing) {
-		genPoints = new ArrayList<Point2D>();
+		injectedPoints = new ArrayList<Point2D>();
 		for(int c = 0; c < waypoints.size() - 1; c++) {
 			Point2D initial = waypoints.get(c);
 			Point2D terminal = waypoints.get(c + 1);
@@ -35,11 +42,58 @@ public class Path extends ArrayList<Point2D> {
 				double x = line.getX1() + (spaced.getComponent().getX() * i);
 				double y = line.getY1() + (spaced.getComponent().getY() * i);
 				Point2D point = new Point2D.Double(x, y);
-				genPoints.add(point);
+				injectedPoints.add(point);
 			}
 		}	
-		genPoints.add(waypoints.get(waypoints.size() - 1));
-		return genPoints;
+		injectedPoints.add(waypoints.get(waypoints.size() - 1));
+		return injectedPoints;
 	}
+	
+	/**
+	 * Takes path and smooths around the cusps by a given radius.
+	 * Higher radius leads to more smoothing, but also more deviation 
+	 * from original path.
+	 * 
+	 * @author Locke Adams
+	 * @param radius
+	 */
+	public ArrayList<Point2D> smooth(int radius) {
+		for(int c = 0; c < waypoints.size() - 2; c++) {
+			if(injectedPoints == null) {
+				Path nullInjected = new Path(waypoints);
+				injectedPoints = nullInjected.injectPoints(0.5);
+			}
+			smoothPoints = new ArrayList<Point2D>(injectedPoints);
+			Point2D cusp = new Point2D.Double();
+			cusp = waypoints.get(c + 1);
+			int cuspIndex = injectedPoints.indexOf(cusp);
+			Point2D beforePoint = injectedPoints.get(cuspIndex - 1);
+			Point2D afterPoint = injectedPoints.get(cuspIndex + 1);
+			double beforeDeltaX = cusp.getX() - beforePoint.getX();
+			double beforeDeltaY = cusp.getY() - beforePoint.getY();
+			double afterDeltaX = afterPoint.getX() - cusp.getX();
+			double afterDeltaY = afterPoint.getY() - cusp.getY();
+			double cuspDeltaX = (beforeDeltaX + afterDeltaX) / 2; //cuspDeltaX and Y average the lines before and after to approximate the slope of the cusp
+			double cuspDeltaY = (beforeDeltaY + afterDeltaY) / 2;
+			//this averages slope of the points +/- radius away from the current point and changes the current point based on that average slope
+			System.out.println("radius: " + radius);
+			for(int i = 1; i < 2 * radius + 1; i++) {
+				int skipCuspConst = 1; //allows us to skip changing the location of the cusp
+				if(i < radius + 1) {
+					skipCuspConst = 1;
+				} else {
+					skipCuspConst = 0;
+				}
+				int currentPointIndex = cuspIndex - radius + (i - skipCuspConst);
+				double newDeltaX = ((2 * radius - i) * beforeDeltaX + cuspDeltaX + (i - 1) * afterDeltaX) / (2 * radius);
+				double newDeltaY = ((2 * radius - i) * beforeDeltaY + cuspDeltaY + (i - 1) * afterDeltaY) / (2 * radius);
+				double newX = injectedPoints.get(currentPointIndex).getX() + newDeltaX;
+				double newY = injectedPoints.get(currentPointIndex).getY() + newDeltaY;
+				Point2D newPoint = new Point2D.Double(newX, newY);
+				smoothPoints.set(currentPointIndex, newPoint);
+			}
+		}
+		return smoothPoints;
+	} 
 	
 }
